d18d0c71ecc9daa03be5f920fa2407100a946744
razumolla <rajuahmed9344@gmail.com>
2022-12-05 00:06:47 +0600

    dynamic routing

---
+++ pages/blog/blog1.js
@@ -0,0 +1,9 @@
+function blog1() {
+    return (
+        <div>
+            <h2>blog1 </h2>
+        </div>
+    );
+};
+
+export default blog1;
---
+++ pages/blog/index.js
@@ -0,0 +1,10 @@
+function Index() {
+    return <>
+        <h2>This is a index</h2>
+        <h1> blog 1</h1>
+        <h1> blog 2</h1>
+        <h1> blog 3</h1>
+        <h1> blog 4</h1>
+    </>
+}
+export default Index;
